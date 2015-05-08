aa97410f044782d739eb0c4983edd5c159910fdb
LadyElle <elle.stafford@gmail.com>
2015-05-07 21:27:33 -0700

    report page has an extra add option

--- Models/Report.cs
+++ Models/Report.cs
@@ -6,7 +6,7 @@ using System.IO;
 using System.Windows.Forms;
 using System.Drawing;
 using System.Drawing.Printing;
-using CubeReportingModule.Cache.WarehouseCache;
+using CubeReportingModule.Cache;
 
 namespace CubeReportingModule.Models
 {
