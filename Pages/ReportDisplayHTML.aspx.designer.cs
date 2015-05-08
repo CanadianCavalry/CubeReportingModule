107ec9567305216d2b4c041dcc469165eef759ce
CanadianCavalry <CanadianCavalry@gmail.com>
2015-05-08 03:11:58 -0700

    Finished report preview demo page

--- Pages/ReportDisplayHTML.aspx.designer.cs
+++ Pages/ReportDisplayHTML.aspx.designer.cs
@@ -31,12 +31,21 @@ namespace CubeReportingModule.Pages {
         protected global::System.Web.UI.WebControls.Button Email;
         
         /// <summary>
-        /// ReportDisplay control.
+        /// SqlDataSource1 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.ListView ReportDisplay;
+        protected global::System.Web.UI.WebControls.SqlDataSource SqlDataSource1;
+        
+        /// <summary>
+        /// Display control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ListView Display;
     }
 }
