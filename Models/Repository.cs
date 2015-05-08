c272f27be20cdd09b4f700d8632aff80205358c2
Nicolaj Bunting <steelmagnum@hotmail.com>
2015-05-07 21:42:51 -0700

    Added Repository class

---
+++ Models/Repository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CubeReportingModule.Models
+{
+    public class Repository
+    {
+        private AppContext context = new AppContext();
+
+        public IEnumerable<Report> Reports
+        {
+            get { return context.Reports; }
+        }
+    }
+}
--- Pages/Menu.aspx.designer.cs
+++ Pages/Menu.aspx.designer.cs
@@ -40,15 +40,6 @@ namespace CubeReportingModule.Pages {
         protected global::System.Web.UI.WebControls.Button Help;
         
         /// <summary>
-        /// ReportButtonsRepeater control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Repeater ReportButtonsRepeater;
-        
-        /// <summary>
         /// Accounts control.
         /// </summary>
         /// <remarks>
