9cfbeba66c0136c451760a206fe4bcea0b848d49
LadyElle <elle.stafford@gmail.com>
2015-05-28 13:26:40 -0700

    updating html to excel

--- Pages/ReportDisplayHTML.aspx.cs
+++ Pages/ReportDisplayHTML.aspx.cs
@@ -11,7 +11,9 @@ namespace CubeReportingModule.Pages
 {
     public partial class ReportDisplayHTML : System.Web.UI.Page
     {
-        public string reportName = @"C:\GainReport.xlsx";
+
+        public string reportName = @"C:\Users\M\Documents\School\Term6\GainReport1.xlsx";
+
 
         protected void Page_Load(object sender, EventArgs e)
         {
@@ -27,50 +29,71 @@ namespace CubeReportingModule.Pages
 
         public void CreateExcelButton_Click(object sender, EventArgs e)
         {
-            List<List<string>> allRows = new List<List<string>>();
-            for(int i = 1; i < 5; i++)
-            {
-                List<string> row = new List<string>();
-                for (int j = 1; j < 5; j++)
-                {
-                    row.Add(String.Format("{0}", j));
-                }
-                allRows.Add(row);
-            }
-            ExcelConverter.WriteExcelFile(reportName, "test", allRows);
-        }
-
-        public void CreatePdfButton_Click(object sender, EventArgs e)
-        {
             //// Get the current page HTML string by rendering into a TextWriter object
-            //TextWriter outTextWriter = new StringWriter();
-            //HtmlTextWriter outHtmlTextWriter = new HtmlTextWriter(outTextWriter);
-            //base.Render(outHtmlTextWriter);
+            TextWriter outTextWriter = new StringWriter();
+            HtmlTextWriter outHtmlTextWriter = new HtmlTextWriter(outTextWriter);
+            base.Render(outHtmlTextWriter);
 
             //// Obtain the current page HTML string
-            //string currentPageHtmlString = outTextWriter.ToString();
+            string currentPageHtmlString = outTextWriter.ToString();
 
-            //// Create a HTML to PDF converter object with default settings
-            //HtmlToPdfConverter htmlToPdfConverter = new HtmlToPdfConverter();
+            List<string> row = new List<string>();
+            row.Add(outTextWriter.ToString());
+            
+            List<List<string>> allRows = new List<List<string>>();
+            allRows.Add(row);
 
-            //// Start reading document from the top of the table
-            //string htmlElementSelector = "#pdfmarker";
-            //htmlToPdfConverter.RenderedHtmlElementSelector = htmlElementSelector;
+            //List<string> row = new List<string>();
+           // row.Add(currentPageHtmlString);
+            //allRows.Add(row);
 
-            //// Store the pdf data into a buffer
-            //byte[] outPdfBuffer = htmlToPdfConverter.ConvertHtml(currentPageHtmlString, HttpContext.Current.Request.Url.AbsoluteUri);
+            ExcelConverter.WriteExcelFile(reportName, "report1", allRows);
 
             //// Set response content type
-            //Response.AddHeader("Content-Type", "application/pdf");
+            //Response.AddHeader("Content-Type", "application/xlsx");
 
             //// Instruct the browser to open the PDF file as an attachment or inline
-            //Response.AddHeader("Content-Disposition", String.Format("attachment; filename=Partially_Convert_HTML.pdf; size={0}", outPdfBuffer.Length.ToString()));
+            //Response.AddHeader("Content-Disposition", String.Format("attachment; filename=GAINReport.pdf; size={0}"));
 
             //// Write the PDF document buffer to HTTP response
             //Response.BinaryWrite(outPdfBuffer);
 
             //// End the HTTP response and stop the current page processing
             //Response.End();
+
+        }
+
+        public void CreatePdfButton_Click(object sender, EventArgs e)
+        {
+           // //// Get the current page HTML string by rendering into a TextWriter object
+           // TextWriter outTextWriter = new StringWriter();
+           // HtmlTextWriter outHtmlTextWriter = new HtmlTextWriter(outTextWriter);
+           // base.Render(outHtmlTextWriter);
+
+           // //// Obtain the current page HTML string
+           // string currentPageHtmlString = outTextWriter.ToString();
+
+           // //// Create a HTML to PDF converter object with default settings
+           // HtmlToPdfConverter htmlToPdfConverter = new HtmlToPdfConverter();
+
+           // //// Start reading document from the top of the table
+           // string htmlElementSelector = "#pdfmarker";
+           // htmlToPdfConverter.RenderedHtmlElementSelector = htmlElementSelector;
+
+           // //// Store the pdf data into a buffer
+           // byte[] outPdfBuffer = htmlToPdfConverter.ConvertHtml(currentPageHtmlString, HttpContext.Current.Request.Url.AbsoluteUri);
+
+           // //// Set response content type
+           // Response.AddHeader("Content-Type", "application/pdf");
+
+           // //// Instruct the browser to open the PDF file as an attachment or inline
+           //Response.AddHeader("Content-Disposition", String.Format("attachment; filename=Partially_Convert_HTML.pdf; size={0}", outPdfBuffer.Length.ToString()));
+
+           // //// Write the PDF document buffer to HTTP response
+           // Response.BinaryWrite(outPdfBuffer);
+
+           // //// End the HTTP response and stop the current page processing
+           // Response.End();
         }
     }
 }
