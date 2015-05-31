23367cd21a7b9811923a808292f6b43e08c0e504
CanadianCavalry <CanadianCavalry@gmail.com>
2015-05-31 15:02:47 -0700

    Reset Password added

---
+++ Admin/ResetPasswords.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Pages/Site.Master" AutoEventWireup="True" CodeBehind="ResetPasswords.aspx.cs" Inherits="CubeReportingModule.Admin.ResetPasswords" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="HeadContent" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="MainPane" runat="server">
+    <p>
+        <asp:Label ID="ActionStatus" runat="server" CssClass="StatusMessage"></asp:Label> 
+    </p>
+    <asp:DropDownList runat="server" AutoPostBack="true" ID="UserResetList"
+         DataTextField="UserName" DataValueField="UserName" OnSelectedIndexChanged="UserResetList_SelectedIndexChanged">
+    </asp:DropDownList>
+    <br />
+    <br />
+    <asp:Button ID="ResetPasswordButton" runat="server" Text="Reset Password" OnClick="ResetPasswordButton_Click" OnClientClick="return confirm('Reset users password?)" />
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="BottomPane" runat="server">
+</asp:Content>
---
+++ Admin/ResetPasswords.aspx.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CubeReportingModule.Admin
+{
+    public partial class ResetPasswords : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            BindUsersToList();
+
+            if (IsPostBack)
+            {
+                UserResetList.SelectedValue = Request.Form[UserResetList.UniqueID];
+            }
+        }
+
+        private void BindUsersToList()
+        {
+             // Get all of the user accounts
+            MembershipUserCollection users = Membership.GetAllUsers();
+            users.Remove("deus");
+            users.Remove(Membership.GetUser().UserName);
+
+            // If the logged in user is not a SysAdmin, hide all the Admin users
+            if (!Roles.IsUserInRole("SysAdmin"))
+            {
+                string[] admins = Roles.GetUsersInRole("Admin");
+                foreach (string userName in admins)
+                {
+                    users.Remove(userName);
+                }
+            }
+            UserResetList.DataSource = users;
+            UserResetList.DataBind();
+        }
+
+        protected void ResetPasswordButton_Click(object sender, EventArgs e)
+        {
+            MembershipUser selectedUser = Membership.GetUser(UserResetList.SelectedItem.Text);
+            selectedUser.ResetPassword();
+        }
+    }
+}
